57322156f3d681e7d2f85bdab3d2ed457b24cdf1
Andrew Hinh <ajhinh@gmail.com>
2023-05-21 07:23:37 -0700

    Install OpenAI

--- index.js
+++ index.js
@@ -35,3 +35,17 @@ tellme_button.addEventListener('click', () => {
   let username = input_text.value;
   greet(username);
 });
+
+/*
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const response = await openai.createCompletion({
+  model: "text-davinci-003",
+  prompt: "Say this is a test",
+  temperature: 0,
+  max_tokens: 7,
+});
+*/
