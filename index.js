2bd9944a21cb996d091b3d5fe4f88506b688bb9a
Andrew Hinh <ajhinh@gmail.com>
2023-05-23 13:58:02 -0700

    Object refactor

--- index.js
+++ index.js
@@ -7,15 +7,19 @@ const people_div = main_div.querySelector('.people');
 const family = ["andrew", "ethan", "mom"];
 
 // Helper functions
-function greet(name) {
+function Person(name) {
+  this.name = name;
+}
+
+function greet(person) {
   let person_div = document.createElement('div');
   person_div.classname = "person";
   let say_hi = document.createElement('p');
   function hey(name) { return `Hello, ${name}!`; }
   let hi_gif = document.createElement('video');
 
-  if (family.includes(name.toLowerCase())) {
-    say_hi.innerHTML = hey(name[0].toUpperCase() + name.slice(1));
+  if (family.includes(person.name.toLowerCase())) {
+    say_hi.innerHTML = hey(person.name[0].toUpperCase() + person.name.slice(1));
     hi_gif.src = "./assets/momo-happy.mp4";
   } else {
     say_hi.innerHTML = hey("stranger");
@@ -45,8 +49,9 @@ function setTheme() {
 
 // Event listeners
 tellme_button.addEventListener('click', () => {
-  let username = input_text.value;
-  greet(username);
+  let name = input_text.value;
+  let person = new Person(name);
+  greet(person);
 });
 
 theme_button.addEventListener('click', setTheme);
