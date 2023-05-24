dc70dafe0dfe601155e410a7115dc31621cac96b
Andrew Hinh <ajhinh@gmail.com>
2023-05-24 14:42:00 -0700

    Linting

--- src/index.js
+++ src/index.js
@@ -2,66 +2,66 @@
 import "./style.css";
 
 // Helper variables
-const main_div = document.querySelector(".main");
-const theme_button = main_div.querySelector(".theme-button");
-const input_text = main_div.querySelector("input");
-const tellme_button = main_div.querySelector(".tellme-button");
+const mainDiv = document.querySelector(".main");
+const themeButton = mainDiv.querySelector(".theme-button");
+const inputText = mainDiv.querySelector("input");
+const tellMeButton = mainDiv.querySelector(".tellme-button");
 
 // Helper functions
 const Person = (name) => ({ name });
 
 const Momo = (() => {
   const family = ["andrew", "ethan", "mom"];
-  const say_hey = (name) => `Hey ${name}!`;
+  const sayHey = (name) => `Hey ${name}!`;
   const greet = (person) => {
-    const assets_path = "assets/";
-    const people_div = main_div.querySelector(".people");
-    const person_div = document.createElement("div");
-    person_div.classname = "person";
-    const say_hi = document.createElement("p");
-    const hi_gif = document.createElement("video");
+    const assetsPath = "assets/";
+    const peopleDiv = mainDiv.querySelector(".people");
+    const personDiv = document.createElement("div");
+    personDiv.classname = "person";
+    const sayHi = document.createElement("p");
+    const hiGif = document.createElement("video");
 
     if (family.includes(person.name.toLowerCase())) {
-      say_hi.innerHTML = say_hey(
+      sayHi.innerHTML = sayHey(
         person.name[0].toUpperCase() + person.name.slice(1).toLowerCase()
       );
-      hi_gif.src = `${assets_path}momo-bored.mp4`;
+      hiGif.src = `${assetsPath}momo-bored.mp4`;
     } else {
-      say_hi.innerHTML = say_hey("stranger");
-      hi_gif.src = `${assets_path}momo-happy.mp4`;
+      sayHi.innerHTML = sayHey("stranger");
+      hiGif.src = `${assetsPath}momo-happy.mp4`;
     }
 
-    hi_gif.alt = "Momo Gif";
-    hi_gif.autoplay = true;
-    hi_gif.controls = true;
-    hi_gif.loop = true;
-    hi_gif.muted = true;
-    hi_gif.type = "video/mp4";
+    hiGif.alt = "Momo Gif";
+    hiGif.autoplay = true;
+    hiGif.controls = true;
+    hiGif.loop = true;
+    hiGif.muted = true;
+    hiGif.type = "video/mp4";
 
-    person_div.appendChild(say_hi);
-    person_div.appendChild(hi_gif);
-    people_div.appendChild(person_div);
-    main_div.appendChild(people_div);
+    personDiv.appendChild(sayHi);
+    personDiv.appendChild(hiGif);
+    peopleDiv.appendChild(personDiv);
+    mainDiv.appendChild(peopleDiv);
   };
   return { greet };
 })();
 
 function setTheme() {
   const root = document.documentElement;
-  const setTheme = root.className === "dark" ? "light" : "dark";
-  root.className = setTheme;
-  const displayTheme = root.className === "dark" ? "Light" : "Dark";
-  main_div.querySelector(".display-theme-name").textContent = displayTheme;
+  const displayTheme = root.className === "dark" ? "light" : "dark";
+  root.className = displayTheme;
+  const changeTheme = root.className === "dark" ? "Light" : "Dark";
+  mainDiv.querySelector(".display-theme-name").textContent = changeTheme;
 }
 
 // Event listeners
-tellme_button.addEventListener("click", () => {
-  const name = input_text.value;
+tellMeButton.addEventListener("click", () => {
+  const name = inputText.value;
   const person = Person(name);
   Momo.greet(person);
 });
 
-theme_button.addEventListener("click", setTheme);
+themeButton.addEventListener("click", setTheme);
 
 /*
 const { Configuration, OpenAIApi } = require("openai");
