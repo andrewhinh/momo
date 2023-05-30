8d003406a3eb20cda9a9a8617df2686524facdb7
Andrew Hinh <ajhinh@gmail.com>
2023-05-30 09:28:11 -0700

    Backend server creation

---
+++ backend/server.py
@@ -0,0 +1,30 @@
+import openai
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+    return {"message": "What you want?"}
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    try:
+      data = request.json
+      messages = data["messages"]
+      if type(messages) != list or len(messages) == 0:
+        raise Exception
+    except Exception:
+      return {"error": "invalid request body"}, 400
+    
+    try:
+      response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+      )['choices'][0]['message']['content']
+      if not response:
+        raise Exception
+      return {"answer": response}, 200
+    except Exception:
+      return {"error": "OpenAI failure"}, 500
+    
